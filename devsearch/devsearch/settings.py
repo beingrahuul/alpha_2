cc9f78c79e1f5ff7407be75a91f1fbb6f861b545
beingrahuul <bstshownow@gmail.com>
2021-11-29 13:18:52 +0530

    4th

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6@36-ld+-28f0_sy^9ugsh5b&z-lzquu8&%ufgtns!9qdncx4-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,9 +134,10 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,8 +1,22 @@
-
-from django.forms import ModelForm
+from django.forms import ModelForm, fields, widgets
+from django import forms
 from .models import Project
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'description', 'demo_link', 'source_link', 'tags', 'featured_image']
+
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *arg, **kwargs):
+        super(ProjectForm, self).__init__(*arg, **kwargs)
+
+        for k, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input', 'placeholder': k})
+        
+        # self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Title'})
+        # self.fields['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Description'})
+        
